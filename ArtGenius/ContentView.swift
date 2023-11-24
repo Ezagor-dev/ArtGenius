a42ce012567791d1424d4947cc6a17bef2266435
Ata <atakanozalan@gmail.com>
2023-11-25 01:36:09 +0300

    change style ui design

--- ArtGenius/ContentView.swift
+++ ArtGenius/ContentView.swift
@@ -23,53 +23,84 @@ struct ContentView: View {
     @State private var promptHistory: [HistoryItem] = HistoryManager.shared.loadHistory()
     
     var body: some View {
-        NavigationView {
-            VStack {
-                ScrollView {
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text("Enter your prompt:")
-                            .font(.headline)
-                            .foregroundColor(.secondary)
+            NavigationView {
+                ZStack {
+                    // Background gradient from top to bottom
+                    LinearGradient(gradient: Gradient(colors: [Color(hex: "#yourHexValueForTopColor"), Color(hex: "#yourHexValueForTopColor")]), startPoint: .top, endPoint: .bottom)
+                        .ignoresSafeArea()
+                    
+                    // Content
+                    VStack {
+                        Spacer()
+                        
+                        // App title
+                        Text("ArtGenius")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color(hex: "#F4DFC8"))
+                        
+                        // Prompt input area
+                        TextEditor(text: $promptText)
+                            .frame(height: 150)
+                            .padding(4)
+                            .background(Color(hex: "F4DFC8"))
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
                             .padding()
                         
-                        TextEditor(text: $promptText)
-                            .frame(height: 100)
-                            .padding()
-                            .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.secondary, lineWidth: 1))
-                            .padding()
-                    }
-                    
-                    Button("Generate Image") {
-                        isLoading = true
-                        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-                        generateImage()
-                    }
-                    .padding()
-                    .disabled(isLoading)
-                    
-                    if isLoading {
-                        ProgressView("Generating Image...")
-                            .padding()
-                    } else {
-                        ForEach(0..<generatedImages.count, id: \.self) { index in
-                            Image(uiImage: generatedImages[index])
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 300, height: 300)
-                                .onTapGesture {
-                                    showImageDetailView(image: generatedImages[index])
-                                }
+                        // Generate Image Button
+                        Button(action: {
+                            isLoading.toggle()
+                            generateImage()
+                            // Call your generate image function here
+                        }) {
+                            Text(isLoading ? "Loading..." : "Generate Image")
+                                .font(.headline)
+                                .foregroundColor(Color(hex: "F4DFC8"))
+                                .frame(height: 55)
+                                .frame(maxWidth: .infinity)
+                                .background(Color("ButtonColor"))
+                                .cornerRadius(10)
+                                .padding(.horizontal)
                         }
+                        .disabled(isLoading)
+                        
+                        if isLoading {
+                                                ProgressView("Generating Image...")
+                                                    .padding()
+                                            } else {
+                                                ForEach(0..<generatedImages.count, id: \.self) { index in
+                                                    Image(uiImage: generatedImages[index])
+                                                        .resizable()
+                                                        .scaledToFit()
+                                                        .frame(width: 300, height: 300)
+                                                        .onTapGesture {
+                                                            showImageDetailView(image: generatedImages[index])
+                                                        }
+                                                }
+                                            }
+                        
+                        Spacer()
+                        
+                        // Show History Button
+                        NavigationLink(destination: HistoryView(promptHistory: $promptHistory)) {
+                            Text("Show History")
+                                .font(.headline)
+                                .foregroundColor(Color(hex: "F4DFC8"))
+                                .frame(height: 55)
+                                .frame(maxWidth: .infinity)
+                                .background(Color("ButtonColor"))
+                                .cornerRadius(10)
+                                .padding(.horizontal)
+                        }
+                        
+                        Spacer()
                     }
                 }
-                NavigationLink("Show History", destination: HistoryView(promptHistory: $promptHistory))
-                                            .padding()
-                                        
-                                        Spacer() // To ensure the link is not at the bottom of the view
-                                            .navigationBarTitle("ArtGenius", displayMode: .inline)
+                .navigationBarHidden(true)
             }
         }
-    }
+    
     
     private func showImageDetailView(image: UIImage) {
         let imageDetailView = ImageDetailView(image: image)
@@ -186,3 +217,28 @@ struct ContentView: View {
     
     
 
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            opacity: Double(a) / 255
+        )
+    }
+}
